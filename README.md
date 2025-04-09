f3390eae367fe9642cf97a0e6b8e21a0b0ae040d
rosali Johansson <Rosali.alfta@gmail.com>
2025-04-09 16:05:06 +0200

    Created a functional course list add/delete using sqlite3 database server and added info in about

---
+++ README.md
@@ -0,0 +1,5 @@
+## Course list
+
+# This is a web application for storing university courses on a website. You can add and delete your own courses on the website. 
+# The website is created with SQlite3 database-server, and with express, nodemon, ejs and bodyParser.
+# Created by Rosali Johansson for Mid Sweden University April 2025.
--- server.js
+++ server.js
@@ -1,54 +1,93 @@
+
+
+// connection settings  
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const sqlite3 = require("sqlite3").verbose();
 const port = 3000;
-//reads form data
-
+const db = new sqlite3.Database("./db/courses.db");
 
 app.set("view engine", "ejs"); //engine = EJS
 app.use(express.static("public")); //static files in "public"
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//course list array
-const courseList = [];
 
-//route
+//routing
 app.get("/", (req, res) => {
-    res.render("showcourses", { courseList });
+    db.all("SELECT * FROM courses ORDER BY id DESC;", (err, rows) => {
+        if (err) {
+            console.error(err.message);
+        }
+
+        res.render("showcourses", {
+            error: "",
+            rows: rows,
+            saved: ""
+        });
+    });
 });
 
 
 
 app.get("/about", (req, res) => {
-    res.render("about");
+    res.render("about", {
+        error: "",
+        saved: ""
+    });
 });
-
-
-
-app.post("/addcourses", (req, res) => {
-
-    let newName = req.body.coursename
-    let newCode = req.body.coursecode
-    let newSyllabus = req.body.syllabus
-    let newProgression = req.body.progression
-    console.log(newName, newCode, newSyllabus, newProgression)
-    //insert into array
-    courseList.push({
-        name: newName,
-        coursecode: newCode,
-        syllabus: newSyllabus,
-        progression: newProgression
-    }); res.render("addcourses");
-});
-
-
 
 app.get("/addcourses", (req, res) => {
-
-    res.render("addcourses")
+    res.render("addcourses", {
+        error: "",
+        saved: ""
+    });
 });
 
-//starta
+//save new course 
+app.post("/addcourses", (req, res) => {
+
+    let newName = req.body.coursename;
+    let newCode = req.body.coursecode;
+    let newSyllabus = req.body.syllabus;
+    let newProgression = req.body.progression;
+    let error = "";
+    let saved = "";
+
+    if (newName != "" && newCode != "" && newSyllabus != "" && newProgression != "") {
+        //store if correct
+        const stmt = db.prepare("INSERT INTO courses(coursename, coursecode, syllabus, progression)VALUES(?, ?, ?, ?);");
+        stmt.run(newName, newCode, newSyllabus, newProgression);
+        stmt.finalize();
+        saved = "Your course has been saved to 'Your Courses'."
+    } else {// id fields are incorrect
+        error = "Error - Please fill in every field";
+    }
+ 
+
+    res.render("addcourses", {
+        error: error,
+        saved
+    }
+    );
+});
+
+//delete a course
+app.get("/delete/:id", (req, res) => {
+    let id = req.params.id;
+
+    db.run("DELETE FROM courses WHERE id=?;", id, (err) => {
+        if (err) {
+            console.error(err.message)
+        }
+        //redirect
+        res.redirect("/");
+
+    });
+});
+
+
+//start application 
 app.listen(port, () => {
-    console.log("server started on port:" + port);
+    console.log("Application started on port: " + port);
 });
